1578206b92b23db6dd90fd13afb1c53abf1f90e1
114042-TOLEDO-BRUNO <114042@tecnicatura.frc.utn.edu.ar>
2023-08-29 11:34:17 -0300

    added documentation userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -236,7 +236,7 @@ export const deleteUserById = async (req, res, next) => {
     }
 };
 
-export const inverseDeleteUserById = async (req, res, next) => {
+export const inverseDeleteUserById = async (req, res, next) => {//para volver a estar activo el usuario
     try {
         const userId = req.params.id;
 
